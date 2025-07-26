dee9f9c18d5902c72a6daa972b478f8e4d297ec7
Victor Gomes <victorcrepaldigomes@gmail.com>
2025-07-26 17:53:40 -0300

    refactor: ajusted name file .py and add validations in json archive

--- Scraping.py
+++ Scraping.py
@@ -10,16 +10,18 @@ from bs4 import BeautifulSoup
 import time
 import json
 
-
 #Definição da URL do produto
 urls = [
     "https://www.terabyteshop.com.br/produto/34884/placa-de-video-gigabyte-nvidia-geforce-rtx-5070-gaming-oc-12gb-gddr7-dlss-ray-tracing-gv-n5070gaming-oc-12gd",
-    "https://www.kabum.com.br/produto/714574/placa-de-video-gigabyte-rtx-5070-windforce-oc-sff-12g-nvidia-geforce-12gb-gddr7-192bits-dlss-ray-tracing-9vn5070wo-00-g10"
+    "https://www.kabum.com.br/produto/714574/placa-de-video-gigabyte-rtx-5070-windforce-oc-sff-12g-nvidia-geforce-12gb-gddr7-192bits-dlss-ray-tracing-9vn5070wo-00-g10",
+    "https://www.pichau.com.br/placa-de-video-gigabyte-geforce-rtx-5070-gaming-oc-12gb-gddr7-192-bit-gv-n5070gaming-oc-12gd",
+    "https://www.terabyteshop.com.br/produto/28037/water-cooler-gigabyte-aorus-waterforce-ii-360-argb-360mm-intel-amd-black",
+
 ]
 #Criação de um loop infinito para monitorar a página
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 while True:
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     for url in urls:
         print(f"Acessando a URL: {url}")
         #Abre o navegador e acessa a URL
@@ -36,14 +38,33 @@ while True:
         if "terabyteshop" in url:
             name = soup.find('h1', class_='tit-prod')
             name_product = name.text.strip() if name else "Nome não encontrado"
-            price = soup.find('p', class_='val-prod valVista')
+            price = soup.find('p', class_='val-prod valVista') or soup.find('h2', class_='val-prod valVista') or soup.find('i', class_='fa fa-exclamation-circle')
             valor_produto = price.text.strip() if price else "Preço não encontrado"
+
         
         elif "kabum" in url:
-            name = soup.find('h1', class_='tit-prod')  # Ajuste se o nome do produto estiver em outro lugar
+            name = soup.find('h1', class_='text-sm desktop:text-xl text-black-800 font-bold desktop:font-bold')  # Ajuste se o nome do produto estiver em outro lugar
             name_product = name.text.strip() if name else "Nome não encontrado"
             price = soup.find('h4', class_='text-4xl text-secondary-500 font-bold transition-all duration-500')
             valor_produto = price.text.strip() if price else "Preço não encontrado"
+
+        elif "pichau" in url:
+            name = soup.find('h1', class_='MuiTypography-root MuiTypography-h6 mui-vrkxks-product_info_title')  # Ajuste se o nome do produto estiver em outro lugar
+            name_product = name.text.strip() if name else "Nome não encontrado"
+            price = soup.find('div', class_='mui-1q2ojdg-price_vista')
+            valor_produto = price.text.strip() if price else "Preço não encontrado"
+
+        elif "kabum" in url:
+            name = soup.find('h1', class_='text-sm desktop:text-xl text-black-800 font-bold desktop:font-bold')  # Ajuste se o nome do produto estiver em outro lugar
+            name_product = name.text.strip() if name else "Nome não encontrado"
+            price = soup.find('h4', class_='text-4xl text-secondary-500 font-bold transition-all duration-500')
+            valor_produto = price.text.strip() if price else "Preço não encontrado"
+
+        elif "kabum" in url:
+            name = soup.find('h1', class_='text-sm desktop:text-xl text-black-800 font-bold desktop:font-bold')  # Ajuste se o nome do produto estiver em outro lugar
+            name_product = name.text.strip() if name else "Nome não encontrado"
+            price = soup.find('h4', class_='text-4xl text-secondary-500 font-bold transition-all duration-500')
+            valor_produto = price.text.strip() if price else "Preço não encontrado"    
 
         else: 
             name = None
@@ -60,6 +81,7 @@ while True:
             "nome": name_product,
             "preco": valor_produto,
             "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
+            "site": url.split('/')[2],
             "link": url,
         }
 
@@ -76,11 +98,16 @@ while True:
         except (FileNotFoundError, json.JSONDecodeError):
             lista = []
 
-        lista.append(data)
+
+        if not any(item['preco'] == valor_produto and item['nome'] == name_product for item in lista):
+            lista.append(data)
 
         with open("products.json", "w", encoding="utf-8") as f:
             json.dump(lista, f, ensure_ascii=False, indent=4)
 
-        print("Dados salvos com sucesso!", data)
-        time.sleep(10)
-driver.quit()
+        print(f"Dados salvos com sucesso! Nome do produto: {name_product} - Preço: {valor_produto} - Site: {url.split('/')[2]}")
+        time.sleep(5)
+    driver.quit()
+    time.sleep(5)
+    print("Aguardando 5 segundos antes de reiniciar o loop...")
+
